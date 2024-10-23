bb88afa2775ba3170f4d456d55bab547f2c728c9
Manjit Singh <talk2manjit@gmail.com>
2024-10-23 19:30:01 +0530

    First Upload -> All files

--- app/src/main/java/com/example/ourfirstproject/MainActivity.java
+++ app/src/main/java/com/example/ourfirstproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ourfirstproject;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int age = 23;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toast.makeText(this, String.valueOf(age), Toast.LENGTH_SHORT).show();
     }
 }
